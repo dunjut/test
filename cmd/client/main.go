1ace568a9d2a0fc87e6ac2608c1e9772e29636c6
dastan <real.duhw@gmail.com>
2018-09-14 15:50:48 +0800

    init commit of rpc demo

---
+++ cmd/client/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/rpc"
+
+	"github.com/dunjut/test/server"
+)
+
+func main() {
+	client, err := rpc.DialHTTP("tcp", "127.0.0.1:1234")
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+
+	// Synchronous call
+	args := &server.Args{7, 8}
+	var reply int
+	err = client.Call("Arith.Multiply", args, &reply)
+	if err != nil {
+		log.Fatal("arith error:", err)
+	}
+	fmt.Printf("Arith: %d*%d=%d\n", args.A, args.B, reply)
+}
---
+++ cmd/server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+
+	"github.com/dunjut/test/server"
+)
+
+func main() {
+	arith := new(server.Arith)
+	rpc.Register(arith)
+	rpc.HandleHTTP()
+
+	l, e := net.Listen("tcp", ":1234")
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	if e = http.Serve(l, nil); e != nil {
+		log.Fatal("serve error:", e)
+	}
+}
